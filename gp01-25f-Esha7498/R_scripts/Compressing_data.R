c86b22487430a88e4c43e38b90223ff2b78c69a9
Esha7498 <80858877+Esha7498@users.noreply.github.com>
2026-02-22 01:46:15 -0500

    Delete gp01-25f-Esha7498 directory

--- gp01-25f-Esha7498/R_scripts/Compressing_data.R
+++
@@ -1,7 +0,0 @@
-# Load and save compressed
-sba_clean <- readRDS("data/sba_cleaned.rds")
-saveRDS(sba_clean, "data/sba_cleaned_compressed.rds", compress = "xz")
-
-# Check new size
-file.info("data/sba_cleaned_compressed.rds")$size / 1024^2
-
